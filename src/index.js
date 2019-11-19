83c00ee6e16fd50d961ea3d20ac7c8dcc0090dbd
Ori Amir <oamir@diamonds.net>
2019-11-19 15:49:01 +0200

    first upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 // src/index.js
-
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "./react-auth0-spa";
 import config from "./auth_config.json";
-import "core-js"; // <- at the top of your entry point
-
 // A function that routes the user to the right place
 // after login
 const onRedirectCallback = appState => {
